fa2ae5f96b57aaaf9936878a9ebacd2659d50731
kylaris18 <kylaris18@gmail.com>
2020-12-05 02:44:55 +0800

    added crud feature for transactions table

---
+++ backend/components/transactions/transactionsModel.js
@@ -0,0 +1,39 @@
+const sequelize = require('../../helpers/mysql-db-helper');
+const { DataTypes } = require('sequelize');
+
+const Transactions = sequelize.define('transactions', {
+    // Model attributes are defined here
+    transactionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    wholesalerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    goodsId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    deliverySendDate: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: DataTypes.literal('CURRENT_TIMESTAMP'),
+    },
+    deliveryRecieveDate: {
+        type: 'TIMESTAMP',
+        allowNull: true
+    },
+    deliveryDesc: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+    }
+  });
+
+  module.exports = Transactions
