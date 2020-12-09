50ddc0d69f50c8dd6f9adeccc7e63e9f810c1770
mlcruz <mlcruz@unionbankph.com>
2020-12-09 21:14:49 +0800

    Update conract and index

--- backend/contracts/StructStorageABI.js
+++ backend/contracts/StructStorageABI.js
@@ -5,14 +5,57 @@
 			{
 				"indexed": false,
 				"internalType": "uint256",
-				"name": "transactionId",
+				"name": "reviewId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "userId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "score",
 				"type": "uint256"
 			},
 			{
 				"indexed": false,
 				"internalType": "bytes",
-				"name": "status",
+				"name": "reviewDesc",
 				"type": "bytes"
+			}
+		],
+		"name": "decodedReview",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "transactionId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "wholesalerId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "goodsId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "status",
+				"type": "uint256"
 			},
 			{
 				"indexed": false,
@@ -44,6 +87,25 @@
 				"type": "bytes"
 			}
 		],
+		"name": "decodeReview",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes",
+				"name": "_encodedData",
+				"type": "bytes"
+			}
+		],
 		"name": "decodeTransaction",
 		"outputs": [
 			{
@@ -63,6 +125,40 @@
 				"type": "uint256"
 			}
 		],
+		"name": "reviews",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "reviewId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "userId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "score",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "reviewDesc",
+				"type": "bytes"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
 		"name": "transactions",
 		"outputs": [
 			{
@@ -71,9 +167,19 @@
 				"type": "uint256"
 			},
 			{
-				"internalType": "bytes",
+				"internalType": "uint256",
+				"name": "wholesalerId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "goodsId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
 				"name": "status",
-				"type": "bytes"
+				"type": "uint256"
 			},
 			{
 				"internalType": "bytes",
